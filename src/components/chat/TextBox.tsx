b4a02b66bf40fee2c99782772837b42f1761e438
mahdimirzakhani4 <mahdimirzakhani85@gmail.com>
2025-12-12 19:56:30 +0330

    show response

--- src/components/chat/TextBox.tsx
+++ src/components/chat/TextBox.tsx
@@ -6,6 +6,7 @@ import axios from "axios";
 const TextBox = () => {
   const refTextBox = useRef(null);
   const [text, setText] = useState("");
+  const [resData, setResData] = useState<string | undefined>(undefined);
 
   useEffect(() => {
     if (refTextBox.current) {
@@ -16,36 +17,44 @@ const TextBox = () => {
 
   const handleDataChat = async () => {
     const formData = {
-      contents: [
-        {
-          parts: [{ text: "سلام" }],
-        },
-      ],
+      message: text,
     };
     try {
       const res = await axios.post("/api/chat", formData);
       console.log(res?.data);
+      setResData(res?.data?.candidates[0]?.content?.parts[0]?.text);
     } catch (error) {
       console.log(error);
     }
   };
-  console.log(text);
+  console.log(resData);
+
+  const renderText = (text: string) => {
+    return text.replace(/\n/g, "<br/>");
+  };
 
   return (
-    <div className="w-1/2 flex items-end pb-3 justify-center gap-2  border-b border-b-gray-600">
-      <textarea
-        ref={refTextBox}
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-        className="w-full px-3 resize-none overflow-hidden max-h-52  outline-none"
-        placeholder="Ask me anything"
-      />
-      <div
-        onClick={handleDataChat}
-        className="flex items-center cursor-pointer justify-center w-10 h-9 text-xl bg-blue-500 rounded-full"
-      >
-        <IoArrowUp />
+    <div className="flex flex-col items-center justify-center gap-5 w-1/2">
+      <div className="w-full flex items-end pb-3 justify-center gap-2 border-b border-b-gray-600">
+        <textarea
+          ref={refTextBox}
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          className="w-full px-3 resize-none overflow-hidden max-h-52  outline-none"
+          placeholder="Ask me anything"
+        />
+        <div
+          onClick={handleDataChat}
+          className="flex items-center cursor-pointer justify-center w-10 h-9 text-xl bg-blue-500 rounded-full"
+        >
+          <IoArrowUp />
+        </div>
       </div>
+      {resData && (
+        <div className="text-xl w-full mb-20">
+          <div dangerouslySetInnerHTML={{ __html: renderText(resData) }} />
+        </div>
+      )}
     </div>
   );
 };
